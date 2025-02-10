bd1214913fc1fb5a530ec39ada47521caacd12b0
bsekisser <bsekisser@gmail.com>
2025-02-10 18:34:24 -0500

    separate out string data out of header into c file.
    	new file:   source/y86_string.c
    	modified:   source/y86_string.h

--- source/y86_string.h
+++ source/y86_string.h
@@ -1,12 +1,10 @@
-const char* y86_reg_lcase[_y86_reg_count] = {
-	"rax", "rcx", "rdx", "rbx", "rsp", "rbp", "rsi", "rdi",
-	"r8", "r9", "r10", "r11", "r12", "r13", "r14",
-//	"r15"
-};
+#pragma once
 
-const char* y86_state_lcase[_y86_state_count] = {
-	[state_adr] = "ADR",
-	[state_aok] = "AOK",
-	[state_halt] = "HALT",
-	[state_ins] = "INS",
-};
+/* **** */
+
+#include "y86.h"
+
+/* **** */
+
+extern const char* y86_reg_lcase[_y86_reg_count];
+extern const char* y86_state_lcase[_y86_state_count];
